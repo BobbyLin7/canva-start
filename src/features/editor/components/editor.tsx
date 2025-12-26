111720ccacf41de6bf9eb40ef464e8eaf5360556
BobbyLin7 <bobbylin23@163.com>
2025-12-26 17:43:31 +0800

    feat: add editor layout

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,6 +1,10 @@
 import { Canvas } from "fabric";
 import { useEffect, useRef } from "react";
 import { useEditor } from "@/features/editor/hooks/use-editor";
+import { Footer } from "./footer";
+import { Navbar } from "./navbar";
+import { Sidebar } from "./sidebar";
+import { Toolbar } from "./toolbar";
 
 export const Editor = () => {
 	const { init } = useEditor();
@@ -25,9 +29,20 @@ export const Editor = () => {
 	}, [init]);
 
 	return (
-		<div className="flex h-full">
-			<div ref={workspaceRef} className="h-full flex-1 bg-muted">
-				<canvas ref={canvasRef} />
+		<div className="flex h-full flex-col">
+			<Navbar />
+			<div className="absolute top-[68px] flex h-[calc(100%-68px)] w-full">
+				<Sidebar />
+				<main className="relative flex flex-1 flex-col overflow-auto bg-muted">
+					<Toolbar />
+					<div
+						ref={workspaceRef}
+						className="h-[calc(100%-124px)] flex-1 bg-muted"
+					>
+						<canvas ref={canvasRef} />
+					</div>
+					<Footer />
+				</main>
 			</div>
 		</div>
 	);
---
+++ src/features/editor/components/sidebar.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+	return (
+		<aside className="flex h-full w-[100px] flex-col border-r bg-white">
+			Sidebar
+		</aside>
+	);
+};
